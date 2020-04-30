4e87eaeca22c1d1cf99cdf763e18ab7397d5721c
Evan Morrison <evan.morrison@tazworks.com>
2020-04-29 18:22:27 -0600

    update deps & config

--- dist/esm/index.js
+++ dist/esm/index.js
@@ -1,15 +1,12 @@
 import { isNil } from "lodash";
-
 const Translator = {
   defaultLanguageKey: null,
   defaultLanguageLibrary: null,
-
   language: null,
-
   languages: {},
 
   changeLanguage(language) {
-    if(!isNil(this.languages[language])) {
+    if (this.languages.hasOwnProperty(language)) {
       this.language = language;
     } else {
       this.language = this.defaultLanguageKey;
@@ -17,62 +14,65 @@ const Translator = {
   },
 
   findTranslation(translation, id) {
-    const idPieces = id.split(".");
+    let idPieces = id.split(".");
 
-    for(let i = 0; i < idPieces.length; i++) {
-      const piece = idPieces[i];
-      if(isNil(translation[piece])) {
-        return(null);
+    for (let i = 0; i < idPieces.length; i++) {
+      let piece = idPieces[i];
+
+      if (isNil(translation[piece])) {
+        return null;
       } else {
         translation = translation[piece];
       }
     }
 
-    return(translation);
+    return translation;
   },
 
   registerDefaultLanguage(key, library) {
     this.defaultLanguageKey = key;
     this.defaultLanguageLibrary = library;
-    if(this.language === null) {
+
+    if (this.language === null) {
       this.language = this.defaultLanguageKey;
     }
+
     this.registerLanguage(key, library);
   },
 
   registerLanguage(key, library) {
-    const languages = this.languages;
+    let languages = this.languages;
     languages[key] = library;
     this.languages = languages;
   },
 
   translate(id, values = {}) {
-    const library = this.getLibrary();
-    if(typeof(library) === "undefined") {
-      return(id);
+    let library = this.getLibrary();
+
+    if (typeof library === "undefined") {
+      return id;
     }
 
     let translation = this.findTranslation(library, id);
 
-    // defaults
-    if(isNil(translation) && this.language !== this.defaultLanguageKey) {
+    if (isNil(translation) && this.language !== this.defaultLanguageKey) {
       translation = this.findTranslation(this.getLibrary(this.defaultLanguageKey), id);
     }
-    if(isNil(translation)) {
+
+    if (isNil(translation)) {
       translation = id;
     }
 
-    // interpolate
-    for(const key in values) {
+    for (let key in values) {
       translation = translation.split("${" + key + "}").join(values[key]);
     }
 
-    return(translation);
+    return translation;
   },
 
   getLibrary(language = this.language) {
-    return(this.languages[language]);
+    return this.languages[language];
   }
-};
 
+};
 export default Translator;
